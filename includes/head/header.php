0cf60a5babadc0c0acd06e4f0853e173e1dee5ee
Joshua Isaac <jayisaac0@gmail.com>
2019-07-22 14:34:20 +0300

    Update header.php

--- includes/head/header.php
+++ includes/head/header.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <base href="http://localhost/newshopupSite/">
+    
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <!-- Meta, title, CSS, favicons, etc. -->
     <meta charset="utf-8">
