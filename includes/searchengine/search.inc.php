f51c56fd26569d7c5982d72b481f7aa3e7d75ec9
Joshua Isaac <jayisaac0@gmail.com>
2019-07-22 14:30:20 +0300

    Update search.inc.php

--- includes/searchengine/search.inc.php
+++ includes/searchengine/search.inc.php
@@ -5,7 +5,7 @@
         }
 
         if (!empty($search_text)) {
-            if($connect = mysqli_connect('localhost', 'root', 'Jayluv3139', 'newsite' )) {
+            if($connect = mysqli_connect('db4free.net', 'shophapo', 'Jayluv3139', 'shophapo' )) {
             $query = "SELECT * FROM `users` JOIN `shops` ON `users`.`user_id`=`shops`.`user_id`  WHERE `shopname` LIKE '%".mysqli_real_escape_string($connect, $search_text)."%' LIMIT 8 ";
             $query_run = mysqli_query($connect, $query);
 
